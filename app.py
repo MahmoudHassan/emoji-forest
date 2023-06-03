e1c2e77576e42e52022e7e93a99b8f53eff74756
egy-logic <126195487+egy-logic@users.noreply.github.com>
2023-06-03 09:11:08 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,222 +1,283 @@
 import dash
-from dash import dcc, html
-from dash.dependencies import Input, Output 
-import plotly.graph_objects as go
-import statistics
+from dash import dcc, html, Dash, no_update
+from dash.dependencies import Input, Output, State
+import pandas as pd
 import numpy as np
+import plotly.graph_objects as go
+import dash_bootstrap_components as dbc
 
-app = dash.Dash(__name__)
-app.css.append_css({'external_url': 'styles.css'})
+import dash_table
 
-# Define custom CSS styles for the input boxes
-input_box_style = {
-    'width': '80px',
-    'height': '30px',
-    'border': '2px solid #FFD700',
-    'borderRadius': '5px',
-    'textAlign': 'center',
-    'marginRight': '10px',
-    'fontSize': '16px',
-    'fontWeight': 'bold',
-    'color': '#FFD700',
-    'backgroundColor': '#000000',
-}
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app.title="🌳 Emoji Forest Statistics 🌲"
+# Define initial empty DataFrame
+df = pd.DataFrame(columns=['Height of Emoji Tree'])
 
-app.layout = html.Div(
-    style={'padding': '20px'}
-)
+app.layout = dbc.Container([
+    html.H1("🌳 Emoji Forest Statistics 🌲"),
 
-header_style = {
-    'fontSize': '32px',
-    'fontWeight': 'bold',
-    'marginBottom': '20px'
-}
-
-story_style = {
-    'backgroundColor': '#F2F2F2',
-    'borderRadius': '5px',
-    'padding': '20px',
-    'marginBottom': '20px',
-    'textAlign': 'center'
-}
-
-apples_div_style = {
-    'display': 'flex',
-    'alignItems': 'center',
-    'marginBottom': '20px'
-}
-
-person_emoji_style = {
-    'fontSize': '20px',
-    'verticalAlign': 'middle',
-    'marginRight': '10px'
-}
-
-friend_box_style = {
-    'display': 'flex',
-    'alignItems': 'center',
-    'marginRight': '20px'
-}
-
-graph_style = {
-    'marginBottom': '20px',
-    'padding': '5px'
-}
-
-mode_div_style = {
-    'backgroundColor': '#F2F2F2',
-    'borderRadius': '5px',
-    'padding': '20px',
-    'textAlign': 'center',
-    'fontWeight': 'bold',
-}
-dynamic_text_style = {
-    'fontWeight': 'bold',
-    'color': '#FF0000',
-}
-app.title = "Mean, Median, and Mode of Apples"
-app.layout = html.Div([
-    html.H1('Mean, Median, and Mode of Apples', style=header_style),
-
-    html.Div([
-        html.Div(id='mean-explanation', style=story_style),
-
-        html.Div([
-            html.Div([
-                html.Div('You:', style={'marginRight': '10px', 'fontSize': '20px', 'fontWeight': 'bold'}),
-                dcc.Input(id='you-input', type='number', value=10, min=0, style=input_box_style),
-            ], style={'display': 'flex', 'alignItems': 'center'}),
-
-            *[html.Div([
-                html.Span("👤", style=person_emoji_style),
-                html.Div(f"Friend {i+1}:", style={'fontSize': '20px', 'fontWeight': 'bold'}),
-                dcc.Input(id=f'friend{i+1}-input', type='number', value=5, min=0, style=input_box_style)
-            ], style=friend_box_style) for i in range(6)]
-        ], style=apples_div_style),
-
-        html.Div(dcc.Graph(id='bar-chart'), style=graph_style),
-    ])
-
-])
-@app.callback(
-    Output('bar-chart', 'figure'),
-    [Input('you-input', 'value')] + [Input(f'friend{i+1}-input', 'value') for i in range(6)]
-)
-def update_graph(you, *friends):
-    # Check if any input is None
-    if None in (you, *friends):
-        return dash.no_update
-
-    names = ["You"] + [f"Friend {i+1}" for i in range(6)]
-    apples = [you] + list(friends)
-
-    mean_apples = sum(apples) / len(apples)
-    mode_apples = statistics.multimode(apples)
-    median_apples = statistics.median(apples)
-
-    fig = go.Figure(data=[
-        go.Scatter(x=names, y=[mean_apples]*len(names), mode='lines', name='Mean Apples', fill='tozeroy', fillcolor='rgba(255, 255, 0, 0.6)'),
-        go.Scatter(x=names, y=[median_apples]*len(names), mode='lines', name='Median Apples', line=dict(color='green', width=2))
-    ])
-
-    for i, name in enumerate(names):
-        for j in range(int(apples[i])):
-            fig.add_annotation(
-                x=name,
-                y=j+1,
-                text="🍎",
-                showarrow=False,
-                font=dict(
-                    size=15,
-                    color="Black"
-                ),
-            )
-
-    x_range = np.arange(len(names))
-
-    if len(set(apples)) == len(apples):
-        mode_text = "N/A"
-    else:
-        mode_text = ", ".join(str(mode) for mode in mode_apples)
-
-        # Find all indices of mode points
-        mode_indices = [i for i, apple in enumerate(apples) if apple in mode_apples]
-
-        # Display finger pointing emoji and text for all mode points
-        for idx in mode_indices:
-            fig.add_annotation(
-                x=names[idx],
-                y=apples[idx],
-                xref="x",
-                yref="y",
-                text="👇",
-                opacity=0.8,
-                showarrow=True,
-                arrowhead=2,
-                arrowcolor="black",
-                arrowsize=1,
-                arrowwidth=1,
-            )
-            fig.add_annotation(
-                x=names[idx],
-                y=apples[idx],
-                xref="x",
-                yref="y",
-                text=f"The mode: {apples[idx]}",
-                showarrow=False,
-                font=dict(
-                    size=12,
-                    color="blue"
-                ),
-                yshift=20
-            )
-
-    fig.update_layout(title='Number of Apples per Person', xaxis_title='Names', yaxis_title='Number of Apples',
-                      yaxis=dict(range=[0, max(apples)+1]))
-
-    return fig
-@app.callback(
-    Output('mean-explanation', 'children'),
-    [Input('you-input', 'value')] + [Input(f'friend{i+1}-input', 'value') for i in range(6)]
-)
-def update_mean_explanation(you, *friends):
-    # Check if any input is None
-    if None in (you, *friends):
-        return dash.no_update
-    
-    total_people = 1 + len(friends)
-    total_apples = you + sum(friends)  
-    mean_apples = (total_apples + you) / total_people
-    mode_apples = statistics.multimode([you] + list(friends))
-    median_apples = statistics.median([you] + list(friends))
-
-    explanation = html.Div([
-        html.H4("Apple Story 🍎:"),
-        html.P([
-            "Imagine that we all share our apples together and divide them fairly. In that case, each one of us would have approximately ",
-            html.Span("{:.2f} apples".format(mean_apples), className='dynamic-text'),
-            " on average! That's the power of sharing and fairness, and that's what we call the mean or average! 🥳"
+    dbc.Row([
+        dbc.Col([
+            html.H6("Grow some Emoji Trees in your forest! Select how many:"),
+            dbc.Tooltip(
+                "Move the slider to choose the number of trees you want to grow, "
+                "then click this button to watch them spring up! 🌱",
+                target="generate-button",
+                placement="bottom",
+            ),
+            dcc.Slider(id='slider', min=10, max=20, value=20, step=1),
+            html.Div(id='slider-output-container'),
+            dbc.Button('Grow Emoji Trees! 🌱',
+                       id='generate-button', color="success"),
         ]),
-    ])
+        dbc.Col([
+            html.H6("Found a special tree? Enter its height here (cm):"),
+            dcc.Input(id='numeric-input', type='number', min=0),
+            dbc.Button('Measure Special Tree 📏🌳',
+                       id='add-button', color="warning"),
+            html.Div(id='numeric-output'),
+        ])
+    ], style={'margin-top': '20px'}),
 
-    if len(set([you] + list(friends))) == total_people:
-        explanation.children.append(html.P("There is no mode for the given apple quantities."))
-    else:
-        explanation.children.append(html.P([
-            "The mode value represents the number of apples that appear most often. In this case, the mode is ",
-            html.Span(", ".join(str(mode) for mode in mode_apples), className='dynamic-text'), "🍎"
-        ]))
+    html.Hr(),
 
-    explanation.children.extend([
-        html.P([
-            "The median value represents the middle value when the apple quantities are arranged in ascending order. It's important to note that when the total number of apple quantities is even, the median is calculated by taking the average of the two middle values. On the other hand, when the total number of apple quantities is odd, the median is the middle value directly. 🍎"
+    dbc.Row([
+        dbc.Col([
+            dash_table.DataTable(
+                id='data-table',
+                columns=[{"name": i, "id": i} for i in df.columns],
+                data=df.head(5).to_dict('records'),
+                style_cell={'textAlign': 'center'},
+                page_size=5,
+                style_header={
+                    'backgroundColor': 'rgb(30, 30, 30)',
+                    'color': 'white'
+                },
+            )
         ]),
-        html.P(["The median value for the current data is: ", html.Span(str(median_apples), className='dynamic-text')]),
-        html.P(["The total number of apples is: ", html.Span(str(total_apples), className='dynamic-text')]),
-        html.P(["There are ", html.Span(str(total_people), className='dynamic-text'), " people in total."])
-    ])
+    ], style={'margin-top': '20px'}),
 
-    return explanation
+    html.Hr(),
+
+    dbc.Row([
+        dbc.Col([
+            html.Div(id='histogram-text', style={'text-align': 'center'}),
+            dcc.Graph(id='histogram'),
+            dcc.Graph(id='box-plot'),
+            dcc.Graph(id='scatter-plot')  # Scatter plot with points and mean line
+        ]),
+    ], style={'margin-top': '20px'}),
+
+    html.Hr(),
+
+    dbc.Row([
+        dbc.Col([
+            html.Div(className="stats-widget", id='stats-summary')
+        ]),
+    ], style={'margin-top': '20px'}),
+
+], fluid=True)
+
+
+@app.callback(
+    Output('slider-output-container', 'children'),
+    Input('slider', 'value')
+)
+def update_slider_output(value):
+    return f"You're growing {value} trees! 🌳🌳"
+
+
+@app.callback(
+    Output('numeric-output', 'children'),
+    Input('add-button', 'n_clicks'),
+    State('numeric-input', 'value')
+)
+def add_data(n_clicks, value):
+    global df
+    if n_clicks is not None and n_clicks > 0:
+        # Add specific data point to data
+        df = df.append({'Height of Emoji Tree': value}, ignore_index=True)
+        return f'Height of special tree measured: {value} cm! 📏🌳'
+
+
+@app.callback(
+    Output('data-table', 'data'),
+    Output('data-table', 'columns'),
+    Output('histogram', 'figure'),
+    Output('box-plot', 'figure'),
+    Output('scatter-plot', 'figure'),
+    Output('stats-summary', 'children'),
+    Output('histogram-text', 'children'),
+    Input('generate-button', 'n_clicks'),
+    Input('slider', 'value'),
+    Input('add-button', 'n_clicks'),
+    State('numeric-input', 'value')
+)
+def update_data(n1, n_points, n2, value):
+    global df
+    ctx = dash.callback_context
+    if not ctx.triggered:
+        return no_update, no_update, no_update, no_update, no_update, no_update, no_update
+    else:
+        button_id = ctx.triggered[0]['prop_id'].split('.')[0]
+
+    if button_id == 'generate-button' and n1 > 0:
+        # Generate n_points of random data
+        # Heights of emoji trees
+        data = np.random.normal(loc=150, scale=30, size=n_points)
+        df = pd.DataFrame(data, columns=['Height of Emoji Tree'])
+
+    if df.empty:
+        return no_update, no_update, no_update, no_update, no_update, no_update, no_update
+
+    # Create the plots
+    fig_hist = go.Figure()
+    fig_hist.add_trace(go.Histogram(x=df['Height of Emoji Tree'], nbinsx=20,
+                                    marker_color='green', name='Height', marker_line=dict(color='black', width=1)))
+
+    mean_value = df['Height of Emoji Tree'].mean()
+    median_value = df['Height of Emoji Tree'].median()
+
+    # Add mean and median as separate scatter traces
+    fig_hist.add_trace(go.Scatter(x=[mean_value, mean_value], y=[0, 20], mode='lines',
+                                  line=dict(color='red', width=2), name='Mean'))
+
+    fig_hist.add_trace(go.Scatter(x=[median_value, median_value], y=[0, 20], mode='lines',
+                                  line=dict(color='blue', width=2), name='Median'))
+
+    fig_hist.update_layout(
+        title_text='Histogram: Distribution of Emoji Tree Heights',
+        xaxis_title='Height (cm)',
+        yaxis_title='Count',
+        showlegend=True
+    )
+
+    # Add shape flag based on skewness
+    skewness = df['Height of Emoji Tree'].skew()
+    if skewness < -1:
+        shape_flag = "Left Skew"
+    elif skewness > 1:
+        shape_flag = "Right Skew"
+    else:
+        shape_flag = "Symmetrical"
+
+    # Update histogram text annotation
+    histogram_text = f"Shape: {shape_flag}"
+
+    # Create the box plot
+    fig_box = go.Figure()
+    fig_box.add_trace(go.Box(
+        y=df['Height of Emoji Tree'], boxpoints='outliers', marker_color='darkgreen'))
+
+    fig_box.update_layout(
+        title_text='Box Plot: Emoji Tree Heights',
+        yaxis_title='Height (cm)'
+    )
+
+    # Calculate the statistics
+    range_value = np.ptp(df['Height of Emoji Tree'])
+    q75, q50, q25 = np.percentile(df['Height of Emoji Tree'], [75, 50, 25])
+    iqr = q75 - q25
+    std_dev = np.std(df['Height of Emoji Tree'])
+    variance = np.var(df['Height of Emoji Tree'])
+
+    stats_summary = [
+        html.H4('📊 Emoji Tree Heights Statistics'),
+        html.P(
+            f"Range (difference between highest and lowest height): {range_value:.2f} cm"),
+        html.P(
+            f"Interquartile Range (IQR - range of the middle 50% of heights): {iqr:.2f} cm"),
+        html.P(
+            f"Standard Deviation (measure of height variability): {std_dev:.2f} cm"),
+        html.P(
+            f"Variance (square of standard deviation): {variance:.2f} cm^2"),
+        html.P(f"Shape of the data: {shape_flag}")
+    ]
+
+    # Create the scatter plot
+    fig_scatter = go.Figure()
+
+    # Add scatter points
+    fig_scatter.add_trace(go.Scatter(
+        x=df.index,
+        y=df['Height of Emoji Tree'],
+        mode='markers',
+        marker=dict(
+            size=10,
+            color='green',
+            line=dict(
+                color='black',
+                width=1
+            )
+        ),
+        name='Tree Heights'
+    ))
+
+    # Calculate the upper and lower bounds for the shaded area
+    upper_bound = mean_value + std_dev
+    lower_bound = mean_value - std_dev
+
+    # Add shaded area for one standard deviation
+    fig_scatter.add_shape(
+        type='rect',
+        x0=0,
+        y0=lower_bound,
+        x1=len(df.index) - 1,
+        y1=upper_bound,
+        fillcolor='lightyellow',
+        opacity=0.3,
+        line=dict(
+            color='rgba(0,0,0,0)',
+        ),
+        name='One Standard Deviation'
+    )
+
+    # Add vertical dashed lines to the mean height
+    for index, height in enumerate(df['Height of Emoji Tree']):
+        diff = height - mean_value
+        fig_scatter.add_shape(
+            type="line",
+            x0=index,
+            y0=height,
+            x1=index,
+            y1=mean_value,
+            line=dict(
+                color="red",
+                width=1,
+                dash="dash",
+            ),
+        )
+        fig_scatter.add_annotation(
+            x=index,
+            y=(height + mean_value) / 2,
+            text=f"{diff:.2f} cm",
+            showarrow=False,
+            font=dict(size=10, color='black'),
+            align='center',
+            valign='middle'
+        )
+
+    # Add mean line
+    fig_scatter.add_trace(go.Scatter(
+        x=df.index,
+        y=[mean_value] * len(df),
+        mode='lines',
+        line=dict(color='red', width=2),
+        name='Mean'
+    ))
+
+    fig_scatter.update_layout(
+        title_text='Scatter Plot: Emoji Tree Heights',
+        xaxis_title='Tree',
+        yaxis_title='Height (cm)',
+        showlegend=True
+    )
+
+    # Update data table
+    table_columns = [{"name": i, "id": i} for i in df.columns]
+    table_data = df.to_dict('records')
+
+    return table_data, table_columns, fig_hist, fig_box, fig_scatter, stats_summary, histogram_text
 
 
 if __name__ == '__main__':
